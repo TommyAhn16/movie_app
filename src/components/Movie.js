9ba50aab3d498429ee0cdfef1575c83a4a4b9a99
TommyAhn16 <wooksmail316@gmail.com>
2021-02-26 17:10:54 +0900

    commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -14,8 +14,8 @@ function Movie({ id, year, title, summary, poster, genres }) {
             title,
             summary,
             poster,
-            genres
-          }
+            genres,
+          },
         }}
       >
         <img src={poster} alt={title} title={title} />
@@ -42,7 +42,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
